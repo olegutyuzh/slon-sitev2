72103e94169d59c11b3db80ce7b16ed6dd2682a4
Oleg Utyuzh <oleg.utyuzh@gmail.com>
2026-05-02 11:18:04 +0300

    change icons and add SEO

--- candle.js
+++ candle.js
@@ -1,4 +1,4 @@
-    const btn = document.getElementById("lightCandleBtn");
+/*     const btn = document.getElementById("lightCandleBtn");
     const candle = document.getElementById("candleContainer");
 
     if (btn && candle) {
@@ -9,8 +9,34 @@
         // плавний скрол до свічки
         candle.scrollIntoView({ behavior: "smooth", block: "center" });
       });
-    }
+    } */
 
     function lightCandle() {
       document.querySelector('.scene').classList.add('lit');
     }
+
+
+  const btn = document.getElementById("lightCandleBtn");
+  const candle = document.getElementById("candleContainer");
+
+  if (btn && candle) {
+    btn.addEventListener("click", () => {
+
+      // показати свічку
+      candle.classList.add("is-visible");
+
+      // активувати слона
+      const scene = document.querySelector('.memory-scene');
+
+      btn.addEventListener("click", () => {
+        candle.classList.add("is-visible");
+        scene.classList.add("is-lit");
+      });
+
+      // ефект натиснутої кнопки
+      btn.style.opacity = "0.5";
+
+      // плавний скрол
+      candle.scrollIntoView({ behavior: "smooth", block: "center" });
+    });
+  }    
